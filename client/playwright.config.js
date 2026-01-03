793e345e6b83e7ee121a12b0ecb6ef5d0d435d53
Sameer Kumar <sameerkumar04022006@gmail.com>
2026-01-03 23:57:22 +0530

    ci(e2e): add runtime diagnostics to editor and increase Playwright timeouts

--- client/playwright.config.js
+++ client/playwright.config.js
@@ -1,9 +1,11 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 module.exports = {
   testDir: './e2e/tests',
-  timeout: 30000,
+  timeout: 60000,
   use: {
     headless: true,
-    baseURL: 'http://127.0.0.1:5000'
+    baseURL: 'http://127.0.0.1:5000',
+    actionTimeout: 60000,
+    navigationTimeout: 60000
   }
 };
